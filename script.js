322ddcd4a15794d539166c6fb44ea42e3e670c84
marek judym <judym.marek@gmail.com>
2018-11-04 13:56:40 +0100

    added about section
    added about section, and font weight

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-// $('.carousel').carousel({
-//     interval: 1000
-//   })
+$(document).ready(function(){
+    $('#hamburger').click(function(){
+        $('ul').toggleClass('active');
+        $('nav').toggleClass('active');
+        $('#hamburger').toggleClass('iks');     
+    })
+})
+
+$(window).on('scroll', function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+    else {
+        $('nav').removeClass('black');
+    }
+})
+$(window).on('scroll', function() {
+    if($(window).scrollTop()) {
+        $('#logo').addClass('resize');
+    }
+    else {
+        $('#logo').removeClass('resize');
+    }
+})
